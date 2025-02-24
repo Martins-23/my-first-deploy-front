9e87fcebbab1ddae1831cac7825f87beddcbba54
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-02-24 19:43:56 +0100

    Add big fish picture
    Signed-off-by: Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/fish.svg";
+import viteLogo from "/big_fish.webp";
 import "./App.css";
 
 function App() {
