a6f474b6e9ea18eece189634a7c85aa7edc525da
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2025-02-24 20:59:26 +0100

    Add a fetch to the backend
    Signed-off-by: Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/big_fish.webp";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [res, setRes] = useState("");
+
+  async function getData() {
+    const url = "https://my-first-deploy-back.onrender.com/health";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const res = await response.text();
+      console.log(res);
+      setRes(res);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
@@ -16,7 +37,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Saludos desde Vercel</h1>
+      <h1>{res}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
